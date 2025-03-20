b0a2650c5c4ecaff8c4b310909c68fd339ae88d4
Stephen Tung <stktung@yahoo.com>
2025-03-20 15:43:20 +0800

    Code cleanup for RedisProjection

--- polyglot-persistence/RedisProjection/Program.cs
+++ polyglot-persistence/RedisProjection/Program.cs
@@ -5,6 +5,8 @@ using EventStore.Client;
 using StackExchange.Redis;
 using StreamPosition = EventStore.Client.StreamPosition;
 
+Console.WriteLine($"{AppDomain.CurrentDomain.FriendlyName} started");
+
 var redis = ConnectionMultiplexer.Connect("localhost:6379").GetDatabase();
 var esdb = new EventStoreClient(EventStoreClientSettings.Create("esdb://admin:changeit@localhost:2113?tls=false"));
 
@@ -14,26 +16,26 @@ var streamPosition = long.TryParse(redis.StringGet("checkpoint"), out var checkp
 
 await using var subscription = esdb.SubscribeToStream("$ce-payment", streamPosition, true);
 
+Console.WriteLine($"Subscribing events from stream after {streamPosition}");
+
 await foreach (var message in subscription.Messages)
 {
-    if (message is not StreamMessage.Event(var evnt)) continue;
+    if (message is not StreamMessage.Event(var e)) continue;
     
-    var paymentEvent = JsonSerializer.Deserialize<PaymentEvent>(Encoding.UTF8.GetString(evnt.Event.Data.Span));
+    var @event = JsonSerializer.Deserialize<PaymentEvent>(Encoding.UTF8.GetString(e.Event.Data.Span));
     
-    if (paymentEvent == null) continue;
-    
-    Console.WriteLine($"Processing payment: {paymentEvent.id}, Amount: {paymentEvent.amount} {paymentEvent.currency}");
+    if (@event == null) continue;
 
     var redisTransaction = redis.CreateTransaction();
-    redisTransaction.StringIncrementAsync("payment", (double)(paymentEvent.amount ?? 0));
-    redisTransaction.StringSetAsync("checkpoint", evnt.OriginalEventNumber.ToInt64());
-    redisTransaction.ExecuteAsync();
+    redisTransaction.StringIncrementAsync("payment", (double)@event.amount);
+    redisTransaction.StringSetAsync("checkpoint", e.OriginalEventNumber.ToInt64());
+    redisTransaction.Execute();
+
+    Console.WriteLine($"Incremented redis value for 'payment' by {@event.amount}");
 }
 
 public record PaymentEvent
 {
-    public string? id { get; set; }
     public decimal? amount { get; set; }
-    public string? currency { get; set; }
     public DateTime timeStamp { get; set; }
 }
