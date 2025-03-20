94ef7a5efb653e9495c9f1c14888ee3e561afdea
Stephen Tung <stktung@yahoo.com>
2025-03-20 19:18:41 +0800

    Minor code cleanup

--- polyglot-persistence/RedisProjection/Program.cs
+++ polyglot-persistence/RedisProjection/Program.cs
@@ -10,36 +10,36 @@ Console.WriteLine($"{AppDomain.CurrentDomain.FriendlyName} started");
 var redis = ConnectionMultiplexer.Connect("localhost:6379").GetDatabase();                                          // Connect to Redis
 var esdb = new EventStoreClient(EventStoreClientSettings.Create("esdb://admin:changeit@localhost:2113?tls=false")); // Connect to EventStoreDB
 
-var checkpointValueFromRedis = redis.StringGet("checkpoint");                     // Get the checkpoint value from redis
-var streamPosition = long.TryParse(checkpointValueFromRedis, out var checkpoint)  // Check if it exists and convertible to long
-    ? FromStream.After(StreamPosition.FromInt64(checkpoint))                      // If so, set var to subscribe events from stream after checkpoint
-    : FromStream.Start;                                                           // Otherwise, set var to subscribe events from stream from the start
+var checkpointValue = redis.StringGet("checkpoint");                     // Get the checkpoint value from redis
+var streamPosition = long.TryParse(checkpointValue, out var checkpoint)  // Check if it exists and convertible to long
+    ? FromStream.After(StreamPosition.FromInt64(checkpoint))             // If so, set var to subscribe events from stream after checkpoint
+    : FromStream.Start;                                                  // Otherwise, set var to subscribe events from stream from the start
 
 await using var subscription = 
     esdb.SubscribeToStream(     // Subscribe events..
         "$ce-payment",          // from this stream..
         streamPosition,         // from this position..
-        true);                  // with linked events automatically resolved 
+        true);                  // with linked events automatically resolved (required for system projections)
 
 Console.WriteLine($"Subscribing events from stream after {streamPosition}");
 
 await foreach (var message in subscription.Messages)                    // Iterate through the messages in the subscription
-{                                                                       //
+{                                                                       
     if (message is not StreamMessage.Event(var e)) continue;            // Skip if message is not an event
-                                                                        //
+                                                                        
     var @event = JsonSerializer.Deserialize<PaymentEvent>(              // Deserialize the event
-        Encoding.UTF8.GetString(e.Event.Data.Span));                    //
-                                                                        // 
+        Encoding.UTF8.GetString(e.Event.Data.Span));                    
+                                                                         
     if (@event == null) continue;                                       // Skip if deserialization failed
-                                                                        //          
+                                                                                  
     var txn = redis.CreateTransaction();                                // Create a transaction for Redis       
     txn.StringIncrementAsync("payment", (double)@event.amount);         // Update the Redis read model
     txn.StringSetAsync("checkpoint", e.OriginalEventNumber.ToInt64());  // Set the checkpoint to the current event number
     txn.Execute();                                                      // Execute the transaction
-                                                                        //
-    Console.WriteLine($"Incremented redis read model for 'payment'" +   //
-                                                " by {@event.amount}"); //
-}                                                                       //
+                                                                        
+    Console.WriteLine($"Incremented redis read model for 'payment'" +   
+                                                " by {@event.amount}"); 
+}                                                                       
 
 public record PaymentEvent
 {
