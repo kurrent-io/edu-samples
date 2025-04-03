77dbc296aec34e2db7f9b6bbef7571441c96c1a9
Stephen Tung <stktung@yahoo.com>
2025-04-03 12:44:42 +0800

    Added setup.sh

---
+++ polyglot-persistence/setup.sh
@@ -0,0 +1,5 @@
+unzip /workspaces/developer-bootcamp/polyglot-persistence/data/commerce-data-set.zip -d /workspaces/developer-bootcamp/polyglot-persistence/
+
+chmod +x /workspaces/developer-bootcamp/polyglot-persistence/tools/Kurrent.Extensions.Commerce/linux-x64/edb-commerce
+
+/workspaces/developer-bootcamp/polyglot-persistence/tools/Kurrent.Extensions.Commerce/linux-x64/edb-commerce seed-data-set /workspaces/developer-bootcamp/polyglot-persistence/data.json
