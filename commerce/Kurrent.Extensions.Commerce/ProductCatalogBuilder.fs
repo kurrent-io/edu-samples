b27fb00114d4aeb127e3eb93a9936115976432e4
yreynhout <yves.reynhout@bittacklr.be>
2025-04-03 14:07:19 +0200

    feat: multiple product sources supported

--- commerce/Kurrent.Extensions.Commerce/ProductCatalogBuilder.fs
+++ commerce/Kurrent.Extensions.Commerce/ProductCatalogBuilder.fs
@@ -18,34 +18,116 @@ module ProductCatalogBuilder =
                 )
 
             use! response_stream = url.GetStreamAsync()
-            use file_stream = File.Create("food.parquet")
+            use file_stream = File.Create("open_food_facts.parquet")
+            do! response_stream.CopyToAsync(file_stream)
+        }
+        
+    let private download_amazon_data () =
+        task {
+            let url =
+                Url(
+                    "https://github.com/luminati-io/eCommerce-dataset-samples/raw/refs/heads/main/amazon-products.csv"
+                )
+
+            use! response_stream = url.GetStreamAsync()
+            use file_stream = File.Create("amazon.csv")
+            do! response_stream.CopyToAsync(file_stream)
+        }
+        
+    let private download_walmart_data () =
+        task {
+            let url =
+                Url(
+                    "https://github.com/luminati-io/eCommerce-dataset-samples/raw/refs/heads/main/walmart-products.csv"
+                )
+
+            use! response_stream = url.GetStreamAsync()
+            use file_stream = File.Create("walmart.csv")
             do! response_stream.CopyToAsync(file_stream)
         }
 
     let build (faker: Faker) (configuration: Configuration) (log: ILogger) =
         task {
-            if not (File.Exists "food.parquet") then
-                log.LogInformation("Downloading Open Food Facts data ... this can take some time")
-                do! download_open_food_facts_data ()
-
+            // Ensure the product data is downloaded
+            match configuration.PIM.ProductSource with
+            | ProductSource.OpenFoodFacts ->
+                if not (File.Exists "open_food_facts.parquet") then
+                    log.LogInformation("Downloading Open Food Facts product data ... this can take some time")
+                    do! download_open_food_facts_data ()
+            | ProductSource.Amazon ->
+                if not (File.Exists "amazon.csv") then
+                    log.LogInformation("Downloading Amazon product data ... this can take some time")
+                    do! download_amazon_data ()
+            | ProductSource.Walmart ->
+                if not (File.Exists "walmart.csv") then
+                    log.LogInformation("Downloading Walmart product data ... this can take some time")
+                    do! download_walmart_data ()
+            | _ -> ()
+                    
             let product_count =
                 faker.Random.Int(configuration.PIM.ProductCount.Minimum, configuration.PIM.ProductCount.Maximum)
-
+            
+            log.LogInformation("Generating {ProductCount} products", product_count)
+            
             let! weighted_products =
-                Sql.connect_with_defaults ()
-                |> Sql.query
-                    $"SELECT DISTINCT(product_name[1].text) FROM read_parquet('food.parquet') LIMIT {product_count}"
-                    _.GetString(0)
-                |> TaskSeq.mapi (fun index product_name ->
-                    { Weight = (float32 index)
-                      Product =
-                        { Id = faker.Commerce.Ean13()
-                          Name = product_name
-                          Price = faker.Commerce.Price(0.01m, 1000.00m, 2, "USD")
-                          TaxRate = faker.Random.ArrayElement [| 0.06m; 0.12m; 0.21m |] } }
-                    : PIM.WeightedProduct)
-                |> TaskSeq.toArrayAsync
-
+                match configuration.PIM.ProductSource with
+                | ProductSource.OpenFoodFacts ->
+                    Sql.connect_with_defaults ()
+                    |> Sql.query
+                        $"SELECT DISTINCT(product_name[1].text) FROM read_parquet('open_food_facts.parquet') LIMIT {product_count}"
+                        _.GetString(0)
+                    |> TaskSeq.mapi (fun index product_name ->
+                        { Weight = (float32 index)
+                          Product =
+                            { Id = faker.Commerce.Ean13()
+                              Name = product_name
+                              Price = faker.Commerce.Price(0.01m, 1000.00m, 2, "USD")
+                              TaxRate = faker.Random.ArrayElement [| 0.06m; 0.12m; 0.21m |] } }
+                        : PIM.WeightedProduct)
+                    |> TaskSeq.toArrayAsync
+                | ProductSource.Amazon ->
+                    Sql.connect_with_defaults ()
+                    |> Sql.query
+                        $"SELECT DISTINCT(title) FROM read_csv('amazon.csv') LIMIT {product_count}"
+                        _.GetString(0)
+                    |> TaskSeq.mapi (fun index product_name ->
+                        { Weight = (float32 index)
+                          Product =
+                            { Id = faker.Commerce.Ean13()
+                              Name = product_name
+                              Price = faker.Commerce.Price(0.01m, 1000.00m, 2, "USD")
+                              TaxRate = faker.Random.ArrayElement [| 0.06m; 0.12m; 0.21m |] } }
+                        : PIM.WeightedProduct)
+                    |> TaskSeq.toArrayAsync
+                | ProductSource.Walmart ->
+                    Sql.connect_with_defaults ()
+                    |> Sql.query
+                        $"SELECT DISTINCT(product_name) FROM read_csv('walmart.csv') LIMIT {product_count}"
+                        _.GetString(0)
+                    |> TaskSeq.mapi (fun index product_name ->
+                        { Weight = (float32 index)
+                          Product =
+                            { Id = faker.Commerce.Ean13()
+                              Name = product_name
+                              Price = faker.Commerce.Price(0.01m, 1000.00m, 2, "USD")
+                              TaxRate = faker.Random.ArrayElement [| 0.06m; 0.12m; 0.21m |] } }
+                        : PIM.WeightedProduct)
+                    |> TaskSeq.toArrayAsync
+                | _ ->
+                    [
+                        for index in 1 .. product_count do
+                            let product_name = faker.Commerce.ProductName()
+                            { Weight = (float32 index)
+                              Product =
+                                { Id = faker.Commerce.Ean13()
+                                  Name = product_name
+                                  Price = faker.Commerce.Price(0.01m, 1000.00m, 2, "USD")
+                                  TaxRate = faker.Random.ArrayElement [| 0.06m; 0.12m; 0.21m |] } }
+                            : PIM.WeightedProduct
+                    ]
+                    |> TaskSeq.ofList
+                    |> TaskSeq.toArrayAsync
+                        
             let total_weight = weighted_products |> Array.sumBy _.Weight
 
             let normalized_products =
