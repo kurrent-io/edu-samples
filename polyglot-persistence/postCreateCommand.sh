6f873dfaf8e857c43641d806950e2fe823723160
Stephen Tung <stktung@yahoo.com>
2025-04-03 20:55:28 +0800

    Removed postCreateCommand for now

--- polyglot-persistence/postCreateCommand.sh
+++ polyglot-persistence/postCreateCommand.sh
@@ -1,9 +1,9 @@
-docker compose -f /workspaces/developer-bootcamp/polyglot-persistence/docker-compose.yml up -d
+# docker compose -f /workspaces/developer-bootcamp/polyglot-persistence/docker-compose.yml up -d
 
-dotnet run --project /workspaces/developer-bootcamp/polyglot-persistence/DemoWeb &
+# dotnet run --project /workspaces/developer-bootcamp/polyglot-persistence/DemoWeb &
 
-dotnet run --project /workspaces/developer-bootcamp/polyglot-persistence/PostgresProjection &
+# dotnet run --project /workspaces/developer-bootcamp/polyglot-persistence/PostgresProjection &
 
-dotnet run --project /workspaces/developer-bootcamp/polyglot-persistence/RedisProjection &
+# dotnet run --project /workspaces/developer-bootcamp/polyglot-persistence/RedisProjection &
 
-dotnet run --project /workspaces/developer-bootcamp/polyglot-persistence/MongoProjection &
+# dotnet run --project /workspaces/developer-bootcamp/polyglot-persistence/MongoProjection &
