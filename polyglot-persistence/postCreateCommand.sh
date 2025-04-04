6e0dfa9ba4637097300a27d0b87a8030106972e3
Stephen Tung <stktung@yahoo.com>
2025-04-04 23:25:19 +0800

    Remove postCreate afterall

--- polyglot-persistence/postCreateCommand.sh
+++ polyglot-persistence/postCreateCommand.sh
@@ -1,15 +1,15 @@
-max_attempts=10
-attempt=0
-while ! docker ps > /dev/null 2>&1; do                                # While docker fails to run (e.g. Docker daemon is not running)
-       if [ "$attempt" -ge "$max_attempts" ]; then                    # If number of attempt exceeds the max_attempts then we exit
-              echo "Docker daemon is still not available. Exiting"
-              exit 1
-       fi
-       attempt=$((attempt+1))                                         # Increment the attempt count
-       sleep 2                                                        # Wait for few seconds before we check again
-done
+# max_attempts=10
+# attempt=0
+# while ! docker ps > /dev/null 2>&1; do                                # While docker fails to run (e.g. Docker daemon is not running)
+#        if [ "$attempt" -ge "$max_attempts" ]; then                    # If number of attempt exceeds the max_attempts then we exit
+#               echo "Docker daemon is still not available. Exiting"
+#               exit 1
+#        fi
+#        attempt=$((attempt+1))                                         # Increment the attempt count
+#        sleep 2                                                        # Wait for few seconds before we check again
+# done
 
-docker compose -f /workspaces/developer-bootcamp/polyglot-persistence/docker-compose.yml up -d
+# docker compose -f /workspaces/developer-bootcamp/polyglot-persistence/docker-compose.yml up -d
 
 # dotnet run --project /workspaces/developer-bootcamp/polyglot-persistence/DemoWeb &
 
