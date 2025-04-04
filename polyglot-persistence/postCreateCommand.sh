17443fb5af62a5126fdc6579a36480e41dbca59c
Stephen Tung <stktung@yahoo.com>
2025-04-04 12:05:08 +0800

    Start up docker compose on postCreate once again

--- polyglot-persistence/postCreateCommand.sh
+++ polyglot-persistence/postCreateCommand.sh
@@ -1,4 +1,4 @@
-# docker compose -f /workspaces/developer-bootcamp/polyglot-persistence/docker-compose.yml up -d
+docker compose -f /workspaces/developer-bootcamp/polyglot-persistence/docker-compose.yml up -d
 
 # dotnet run --project /workspaces/developer-bootcamp/polyglot-persistence/DemoWeb &
 
