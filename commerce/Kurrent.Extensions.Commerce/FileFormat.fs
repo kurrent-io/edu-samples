b803e8f7a085e8be6c8f65f47e837420668216f8
Stephen Tung <stktung@yahoo.com>
2025-04-28 20:49:36 +0800

    Data generator is moved to another repo

--- commerce/Kurrent.Extensions.Commerce/FileFormat.fs
+++
@@ -1,5 +0,0 @@
-namespace Kurrent.Extensions.Commerce
-
-type FileFormat =
-    | Json
-    | Zip
--- commerce/Kurrent.Extensions.Commerce/ISimulator.fs
+++
@@ -1,7 +0,0 @@
-namespace Kurrent.Extensions.Commerce
-
-open FSharp.Control
-open NodaTime.Testing
-
-type ISimulator<'Event> =
-    abstract member Simulate: FakeClock -> Configuration -> TaskSeq<StreamName * 'Event>
