1d6e2385629fceb68d562ebd8cd2e7336b4028e4
Stephen Tung <stktung@yahoo.com>
2025-04-30 18:55:26 +0800

    Added scripts for tutorial

---
+++ outbox/scripts/2-create-persistent-subscription.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -n "$CODESPACES" ]; then
+    # In Codespace environment, use the preset project root path
+    root_path="/workspaces/edu-samples/outbox"
+else
+    # Otherwise, assume you are in the project root directory
+    root_path="./"
+fi
+
+"$root_path/scripts/start-db.sh" # Start the database containers
+
+# Create the persistence subscription for the fulfillment group
+curl -i -X PUT -d $'{"startFrom": 0,"resolveLinktos": true}' \
+    http://localhost:2113/subscriptions/%24et-order-placed/fulfillment-group \
+    -u admin:changeit -H "Content-Type: application/json"
