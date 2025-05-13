7e1816e8d1767eed904f840d6a49ee5c05e36c4f
Ronan Kumar <ronankumar95@gmail.com>
2025-05-13 15:01:31 +0100

    Add report projection to project current state

--- time-travel/ReportProjection/Program.cs
+++ time-travel/ReportProjection/Program.cs
@@ -2,10 +2,19 @@
 // Report Projection from KurrentDB
 // =======================================================================================================================
 //
-// TODO: Enter description here
-// 
+// This projection takes Sales events in KurrentDB and projects them into a Report JSON object.
+//
+// It:
+//
+// 1. Connects to KurrentDB
+// 2. Looks at the latest Report JSON file to find the current checkpoint
+// 3. Creates a subscription to the "$et-order-placed" stream in KurrentDB
+// 4. Updates the relevant sales report fields in the ReportReadModel object in response to order-placed events
+// 5. Updates the checkpoint in the ReportReadModel object 
+// 5. Serializes the ReportReadModel object into JSON and overwrites the latest report file
 // =======================================================================================================================
 
+using System.Text.Json;
 using EventStore.Client;
 using ReportProjection;
 using Common;
@@ -13,61 +22,90 @@ using StreamPosition = EventStore.Client.StreamPosition;
 
 Console.WriteLine($"{AppDomain.CurrentDomain.FriendlyName} started");
 
+var readModelPath = Environment.GetEnvironmentVariable("READ_MODEL_PATH") ?? "data/report.json"; // Get the path to the report read model from an environment variable
+
 // -------------------- //
 // Connect to KurrentDB //
 // -------------------- //
 
-var kurrentDbHost = Environment.GetEnvironmentVariable("KURRENTDB_HOST") // Get the KurrentDB host from environment variable
-                    ?? "localhost";                                      // Default to localhost if not set
+var kurrentDbHost = Environment.GetEnvironmentVariable("KURRENTDB_HOST")   // Get the KurrentDB host from environment variable
+                    ?? "localhost";                                             // Default to localhost if not set
 
-var kurrentdb = new EventStoreClient(                                    // Create a connection to KurrentDB
+var kurrentdb = new EventStoreClient(                                           // Create a connection to KurrentDB
                 EventStoreClientSettings.Create(
                   $"esdb://{kurrentDbHost}:2113?tls=false"));
 
 
-// ------------------------------- //
-// Serialize JSON to report object //
-// ------------------------------- //
+// --------------------------------- //
+// Deserialize JSON to report object //
+// --------------------------------- //
 
-// TODO: Serialize JSON to report read model object
-var report = new ReportReadModel();
+var hasExistingReadModel = File.Exists(readModelPath);
+
+var readModel = hasExistingReadModel                                                                                           // Check whether there is an existing read model JSON file...
+    ? JsonSerializer.Deserialize<ReportReadModel>(File.ReadAllText(readModelPath)) ?? new ReportReadModel()               // if so, shen deserialize the report JSON into a record
+    : new ReportReadModel();                                                                                                   // otherwise, initialize a new ReportReadModel
 
 // ---------------------------------------------------------- //
 // Retrieve the last checkpoint position from JSON Read Model //
 // ---------------------------------------------------------- //
 
-// TODO: Retrieve checkpoint from JSON
-long? checkpointValue = 0;                                          // Get the checkpoint value from JSON read model
+var checkpointValue = readModel.Checkpoint;                                     // Get the checkpoint value from the report read model object
     
-var streamPosition = checkpointValue.HasValue                            // Check if the checkpoint exists..
-    ? FromStream.After(StreamPosition.FromInt64(checkpointValue.Value))  // if so, subscribe from stream after checkpoint..
-    : FromStream.Start;                                                  // otherwise, subscribe from the start of the stream
+var streamPosition = hasExistingReadModel
+    ? FromStream.After(StreamPosition.FromInt64(checkpointValue))
+    : FromStream.Start;
 
 // ---------------------------------------------- //
 // Subscribe to KurrentDB from checkpoint onwards //
 // ---------------------------------------------- //
 
-await using var subscription = kurrentdb.SubscribeToStream(              // Subscribe events..
+await using var subscription = kurrentdb.SubscribeToStream(              // Subscribe to events..
     "$et-order-placed",                                                  // from the order placed event type system projection..        
     streamPosition,                                                      // from this position..
-    true);                                                               // with linked events automatically resolved (required for system projections)
+    true);                                                   // with linked events automatically resolved (required for system projections)
 
-Console.WriteLine($"Subscribing events from stream after {streamPosition}");
+Console.WriteLine($"Subscribing to events from stream after {streamPosition}");
 
 // ---------------------------------------- //
 // Process each event from the subscription //
 // ---------------------------------------- //
 
-await foreach (var message in subscription.Messages)                     // Iterate through the messages in the subscription
+await foreach (var message in subscription.Messages)                                            // Iterate through the messages in the subscription
 {
-    if (message is not StreamMessage.Event(var e)) continue;             // Skip this message if it is not an event
+    if (message is not StreamMessage.Event(var e)) continue;                        // Skip this message if it is not an event
 
-    if (EventEncoder.Decode(e.Event.Data, "order-placed")                   // Try to deserialize the event to an OrderPlaced event
-            is not OrderPlaced orderPlaced)                                     // Skip this message if it is not an OrderPlaced event
+    if (EventEncoder.Decode(e.Event.Data, "order-placed")                          // Try to deserialize the event to an OrderPlaced event
+            is not OrderPlaced orderPlaced)                                                     // Skip this message if it is not an OrderPlaced event
             continue;
-    
-    // TODO: update and save report read model based on OrderPlaced event 
 
+    var orderDate = orderPlaced.at!.Value.Date.ToString("yyyy-MM-dd");
+
+    var region = orderPlaced.store!.geographicRegion!;
+    
+    foreach (var lineItem in orderPlaced.lineItems!)
+    {
+        var category = lineItem.category!;
+        
+        var salesReport = readModel.SalesReports.GetValueOrDefault(orderDate, new SalesReport());
+        var categorySalesData = salesReport.CategorySalesReports.GetValueOrDefault(category, new CategorySalesReport());
+        var regionSalesData = categorySalesData.RegionSalesReports.GetValueOrDefault(region, new RegionSalesReport());
+
+        var newSalesData = regionSalesData with
+        {
+            DailySales = regionSalesData.DailySales + lineItem.pricePerUnit!.Value * lineItem.quantity!.Value,
+        };
+        
+        categorySalesData.RegionSalesReports[region] = newSalesData;
+        salesReport.CategorySalesReports[category] = categorySalesData;
+        readModel.SalesReports[orderDate] = salesReport;
+    }
+
+    readModel.Checkpoint = e.Event.EventNumber.ToInt64();                                       // Set the read model checkpoint to the event number of the event we just read
+
+    Directory.CreateDirectory(Directory.GetParent(readModelPath)!.FullName);                    // Create the directory for the report read model if it doesn't exist
+    File.WriteAllText(readModelPath, JsonSerializer.Serialize(readModel));              // Write the report read model to the JSON file
+    
     Console.WriteLine($"Projected event " +
                       $"#{e.OriginalEventNumber.ToInt64()} " +
                       $"{e.Event.EventType}");
