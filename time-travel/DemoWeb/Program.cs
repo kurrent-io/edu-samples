6f11bcd168286f6512ff3e3d657ad8ab9cd61a96
Ronan Kumar <ronankumar95@gmail.com>
2025-05-15 18:22:02 +0100

    Convert Time Travel DemoWeb to ASP.NET

---
+++ time-travel/DemoWeb/Program.cs
@@ -0,0 +1,29 @@
+using System.Text.Json;
+using Common;
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+
+var salesDataFilepath = Environment.GetEnvironmentVariable("SALES_DATA_FILEPATH") ?? "data/report.json";
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/sales-data", () =>
+{
+    if (!File.Exists(salesDataFilepath))
+        throw new FileNotFoundException("Sales read model data not found", salesDataFilepath);
+    
+    var salesDataJson = File.ReadAllText(salesDataFilepath);                // Read the sales report JSON file
+    var salesData = JsonSerializer.Deserialize<ReportReadModel>(salesDataJson);   // Deserialize the JSON into a ReportReadModel object
+    
+    return salesData;
+});
+
+app.UseStaticFiles();
+
+app.MapFallbackToFile("index.html"); // Serve wwwroot/index.html which is built by Vite
+
+app.Run($"http://localhost:{port}");
--- time-travel/DemoWeb/client/vite.config.ts
+++ time-travel/DemoWeb/client/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
+import * as path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, '../wwwroot'),
+    minify: false,
+    emptyOutDir: true
+  }
 })
