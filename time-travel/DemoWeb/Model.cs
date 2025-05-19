dc2d88ef697ca2c216c5a357510705dd72dbdc0e
Stephen Tung <stktung@yahoo.com>
2025-05-19 18:52:50 +0800

    Refactored files for demoweb

---
+++ time-travel/DemoWeb/Model.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoWeb;
+
+public enum SalesFigureType
+{
+    DailySales,
+    TotalMonthlySales
+}
+
+public class OrderEventSummaryForSalesReport
+{
+    public long EventNumber { get; set; }
+    public string OrderId { get; set; } = default!;
+    public DateTimeOffset At { get; set; }
+    public string Region { get; set; } = default!;
+    public string Category { get; set; } = default!;
+    public decimal TotalSalesForCategory { get; set; } = default!;
+}
