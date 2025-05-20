a121f4c22bafc13b143cc72d4664172bdb1e582d
Ronan Kumar <ronankumar95@gmail.com>
2025-05-20 12:06:01 +0000

    Fix TS service not recognising CSS modules

--- time-travel/DemoWeb/client/vite.config.ts
+++ time-travel/DemoWeb/client/vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-import * as path from 'path'
+import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
   build: {
-    outDir: path.resolve(__dirname, '../wwwroot'),
+    outDir: path.resolve(__dirname, "../wwwroot"),
     minify: false,
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+  },
 })
